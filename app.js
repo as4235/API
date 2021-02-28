9eae733d0c42f465ee660761de31fb6ae5cf4dcb
as4235 <as4235@njit.edu>
2021-02-28 17:18:00 -0500

    main file

--- app.js
+++ app.js
@@ -18,10 +18,11 @@ client.connect()
 app.get('/cooe/:id?:idd', async (req, res) => {
     const id1 = req.params.id1;
     const id2 = req.params.id2;
-    try{    
-        // const {rows} = await client.query('SELECT a.teeupid FROM cooe.teeup_users a JOIN cooe.teeup_users b USING (teeupid) WHERE a.usersid = $1 AND b.usersid = $2', [id1, id2])
-        // res.json(rows)
-        res.send({id1, id2})
+    try{
+        //to find common teeups among two users
+        const {rows} = await client.query('SELECT a.teeupid FROM cooe.teeup_users a JOIN cooe.teeup_users b USING (teeupid) WHERE a.usersid = $1 AND b.usersid = $2', [id1, id2])
+        res.json(rows)
+        //res.send({id1, id2})
     }
     catch(e){
         console.log(e)
